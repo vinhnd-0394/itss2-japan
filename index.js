cf06bec23a109ba6de394d3e4518bdcb19df59a6
vinhnd-0394 <ngu.duy.vinh@sun-asterisk.com>
2024-06-04 13:23:14 +0700

    them tailwind

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './src/resources/App';
-
 import { BrowserRouter } from 'react-router-dom';
+
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
