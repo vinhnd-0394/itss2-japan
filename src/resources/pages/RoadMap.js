26f880ed95dbe1529a21599eb54ed5da160ea76d
Tuan Tran <trant5796@gmail.comname>
2024-06-04 13:50:23 +0700

    header

--- src/resources/pages/RoadMap.js
+++ src/resources/pages/RoadMap.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './Header'; // Import the Header component
 
 const RoadMap = () => {
-  return <div>RoadMap Page</div>;
+  
+  return (
+    <div>
+      <Header /> {/* Include the Header component */}
+      <div className="font-bold text-red-500">Roadmap</div>
+    </div>
+  );
 };
 
 export default RoadMap;
