f12d631acc1f7129e4b70a9b73677681e51ddbbe
Tùng Cao Thanh <caothanhtung08082002@gmail.com>
2024-06-13 16:09:14 +0700

    xong api

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -30,6 +30,6 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("Users", userSchema);
 
 module.exports = User;
--- src/routes/result-route.js
+++ src/routes/result-route.js
@@ -11,13 +11,40 @@ router.get("/", async function (req, res) {
   }
 });
 
+router.post("/", async function (req, res) {
+  const questions = req.body.exam.questions;
+  const totalQuestion = questions.length;
+  let totalCorrectAnswers = 0;
+  for (let question of questions) {
+    if (question.userAnswer == question.correctAnswer) {
+      totalCorrectAnswers += 1;
+    }
+  }
+  let score = Math.ceil((totalCorrectAnswers / totalQuestion) * 990);
+  answer = req.body;
+  answer.score = score;
+  answer.numberOfCorrectAnswers = totalCorrectAnswers;
+  answer.totalQuestion = totalQuestion;
+  const result = new Result(answer);
+  try {
+    const savedResult = await result.save();
+    res.status(200).json(savedResult);
+  } catch (err) {
+    console.error("Error saving result:", err);
+    res.status(500).json({ message: "Error save result", error });
+  }
+});
+
 router.get("/by-user/:userId/test/:testId", async function (req, res) {
   try {
+    console.log(
+      "userId: " + req.params.userId + " testId: " + req.params.testId
+    );
     const results = await Result.find({
-      user_id: req.params.userId,
-      test_id: req.params.testId,
+      userId: req.params.userId,
+      examId: req.params.testId,
     });
-    res.status(200).json(results);
+    res.status(200).json(results[results.length - 1]);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving users", error });
   }
