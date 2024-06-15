c0f618be31bc09f6c73475bb7cf290d4cd26583c
vinhnd-0394 <ngu.duy.vinh@sun-asterisk.com>
2024-06-15 13:05:03 +0700

    update

--- src/resources/apis/http.js
+++ src/resources/apis/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://itss2-japan.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
